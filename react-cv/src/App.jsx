1056c0ffae5010c4219f2c9e5f61b1b06b4e6eaa
ThomasDupin <Thomas.dupin@viacesi.fr>
2022-03-07 21:28:57 +0100

    backbone of the project

---
+++ react-cv/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Topbar from './components/topbar/Topbar';
+ 
+
+function App(){
+  return (
+      <div className='App'>
+        <Topbar/>
+        <div className="sections"></div>
+  
+      </div>
+  );
+};
+
+export default App;

32b843198aad4d5be8884d0d9d7cce85f91efe51
ThomasDupin <Thomas.dupin@viacesi.fr>
2022-03-10 16:09:09 +0100

    Portfolio, missing dynamic class and useEffect

--- react-cv/src/components/portfolio/Portfolio.jsx
+++ react-cv/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,49 @@ import React from 'react';
 const Portfolio = () => {
     return (
         <div className='portfolio' id='portfolio'>
+            <h1>Portfolio</h1>
+            <ul>
+                <li className='active'>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+                <li>Featured</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>Premier item</h3>
+                </div>
+
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>Second item</h3>
+                </div>
+
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>Trois item</h3>
+                </div>
+
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>quatre item</h3>
+                </div>
+
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>cinq item</h3>
+                </div>
+
+                <div className="item">
+                    <img src='assets/JV.png' alt=""></img>
+                    <h3>six item</h3>
+                </div>
+
+                
             
+            </div>
         </div>
     );
 };

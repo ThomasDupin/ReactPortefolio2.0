97e0e011bdb788a64f588cb5144712e20900e91d
ThomasDupin <Thomas.dupin@viacesi.fr>
2022-03-12 16:42:27 +0100

    Portfolio page finished

--- react-cv/src/components/portfolio/Portfolio.jsx
+++ react-cv/src/components/portfolio/Portfolio.jsx
@@ -1,50 +1,85 @@
 import React from 'react';
+import PortfolioList from './PortfolioList';
+import { useEffect, useState } from "react";
+import {
+    featuredPortfolio,
+    SchoolPortfolio,
+    InternshipPortfolio,
+    PersonnalPortfolio,
+    OtherPortfolio,
+  } from "../../data";
 
 const Portfolio = () => {
+    const [selected, setSelected] = useState("featured")
+    const [data, setData] = useState([]);
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+          },
+          {
+            id: "School",
+            title: "School project",
+          },
+          {
+            id: "Internship",
+            title: "Internship project",
+          },
+          {
+            id: "Personnal",
+            title: "Personnal project",
+          },
+          {
+            id: "Other",
+            title: "Other",
+          },
+        ];
+    useEffect(() => {
+        switch (selected) {
+            case "featured":
+              setData(featuredPortfolio);
+              break;
+            case "School":
+              setData(SchoolPortfolio);
+              break;
+            case "Internship":
+              setData(InternshipPortfolio);
+              break;
+            case "Personnal":
+              setData(PersonnalPortfolio);
+              break;
+            case "Other":
+              setData(OtherPortfolio);
+              break;
+            default:
+              setData(featuredPortfolio);
+          }
+    },[selected])
+    
+
     return (
         <div className='portfolio' id='portfolio'>
             <h1>Portfolio</h1>
             <ul>
-                <li className='active'>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
-                <li>Featured</li>
+                {list.map((item)=> (
+
+                    <PortfolioList title={item.title} 
+                    active={selected ===item.id} 
+                    setSelected= {setSelected}
+                    id={item.id}
+                    />
+                ))}
             </ul>
             <div className="container">
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>Premier item</h3>
-                </div>
+                {data.map((d) => (
 
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>Second item</h3>
-                </div>
+                    <div className="item">
+                    <img src={d.img} alt=""></img>
+                    <h3>{d.title}</h3>
+                    </div>
 
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>Trois item</h3>
-                </div>
+                ))}
 
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>quatre item</h3>
-                </div>
-
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>cinq item</h3>
-                </div>
-
-                <div className="item">
-                    <img src='assets/JV.png' alt=""></img>
-                    <h3>six item</h3>
-                </div>
-
-                
-            
             </div>
         </div>
     );
--- react-cv/src/components/works/Works.jsx
+++ react-cv/src/components/works/Works.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function Works() {
   return (
     <div className='works' id='works'>
-        
+        <div className="slider">
+            <div className="item">
+              <div className="left"></div>
+              <div className="rig"></div>
+            </div>
+        </div>
     </div>
   )
 }
